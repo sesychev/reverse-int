bc6a985900036fba287ad2c4c43b9084cc808b90
sesychev <sergeysychou@gmail.com>
2022-07-12 21:16:04 +0300

    feat: finish reverse-intr

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-	
+	const str = Math.abs(n).toString();
+	let res = '';
+
+	for (let i = str.length - 1; i >= 0; i--) {
+		res += str[i];
+	}
+
+	return +res;
 }
